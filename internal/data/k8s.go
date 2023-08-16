3418afb93c8dff1a7c56b3245e3898dd0305252c
mouuii <49775493+mouuii@users.noreply.github.com>
2023-08-16 18:17:28 +0800

    k8s repo improve2

--- internal/data/k8s.go
+++ internal/data/k8s.go
@@ -3,9 +3,12 @@ package data
 import (
 	"context"
 	"github.com/go-kratos/kratos/v2/log"
+	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"kubecit/internal/biz"
 
@@ -50,6 +53,27 @@ func (k *k8sRepo) ListPodsByLabelSelector(ctx context.Context, namespace string,
 		return nil, err
 	}
 	return podList, nil
+}
+
+// ListIngress 获取 ingress
+func (k *k8sRepo) ListIngress(ctx context.Context, namespace string) (*networkingv1.IngressList, error) {
+	return k.clientSet.NetworkingV1().Ingresses(namespace).List(ctx, metav1.ListOptions{})
+}
+
+// ListServiceByNamespace 获取  services
+func (k *k8sRepo) ListServiceByNamespace(ctx context.Context, namespace string) (*corev1.ServiceList, error) {
+	return k.clientSet.CoreV1().Services(namespace).List(ctx, metav1.ListOptions{})
+}
+
+// ListEvents 获取事件
+func (k *k8sRepo) ListEvents(ctx context.Context, namespace, uid string) (*corev1.EventList, error) {
+	fieldSelector := fields.OneTermEqualSelector("involvedObject.uid", uid).String()
+	return k.clientSet.CoreV1().Events(namespace).List(ctx, metav1.ListOptions{FieldSelector: fieldSelector})
+}
+
+// GetPodLogReq 获取 pod 日志
+func (k *k8sRepo) GetPodLogReq(pod, namespace string, options *corev1.PodLogOptions) *rest.Request {
+	return k.clientSet.CoreV1().Pods(namespace).GetLogs(pod, options)
 }
 
 func NewK8sRepo(kubeCfg []byte, logger *log.Helper) (biz.K8sRepo, error) {
