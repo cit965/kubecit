4e85c737eae586d508002988ff7cc2cb9d03e6ae
mouuii <49775493+mouuii@users.noreply.github.com>
2023-08-16 15:03:03 +0800

    action

--- internal/biz/biz.go
+++ internal/biz/biz.go
@@ -1,6 +1,4 @@
 package biz
 
-import "github.com/google/wire"
-
 // ProviderSet is biz providers.
-var ProviderSet = wire.NewSet(NewGreeterUsecase, NewUserUsecase)
+var ProviderSet = wwSet(NewGreeterUsecase, NewUserUsecase)
